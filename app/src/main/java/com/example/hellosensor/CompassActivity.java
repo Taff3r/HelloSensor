27b10ff264b326ff698a0a374a96bde70615a399
simon <simon.tenggren@gmail.com>
2019-03-26 18:54:52 +0100

    Fully working

--- app/src/main/java/com/example/hellosensor/CompassActivity.java
+++ app/src/main/java/com/example/hellosensor/CompassActivity.java
@@ -6,21 +6,35 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class CompassActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager sMan;
+    private ImageView compass;
+    private float deg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
         SensorManager sMan = (SensorManager) getSystemService(SENSOR_SERVICE);
-        Sensor mField = sMan.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor mField = sMan.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        this.compass = findViewById(R.id.imgCompass);
+        this.deg = 0f;
         sMan.registerListener(this, mField, SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-
+        float degree = Math.round(event.values[0]);
+        RotateAnimation ra = new RotateAnimation(this.deg, -degree, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        ra.setDuration(200);
+        ra.setFillAfter(true);
+        compass.startAnimation(ra);
+        this.deg = -degree;
     }
 
     @Override
